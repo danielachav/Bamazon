0a19dcfbcecb5ff867a6d52f5d671bf61a7de5ac
Daniela Chavez <danielachavez18@gmail.com>
2019-10-22 12:30:11 -0500

    set up the basics

---
+++ bamazon.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS bamazon;
+
+
+CREATE DATABASE bamazon; 
+
+USE bamazon;
+
+
+CREATE TABLE products (
+
+item_id VARCHAR()
+product_name VARCHAR()
+department_name VARCHAR()
+price DECIMAL() NULL,
+stock_quantity INT NULL,
+PRIMARY KEY (id)
+
+);
+
+
