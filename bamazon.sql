8d5e1fb19c88b32495f727d06a1b3c8056cbb961
Daniela Chavez <danielachavez18@gmail.com>
2019-10-23 09:49:52 -0500

    added 10 products

--- bamazon.sql
+++ bamazon.sql
@@ -17,4 +17,32 @@ PRIMARY KEY (id)
 
 );
 
+INSERT INTO products (name)
+VALUES ("Floor Cleaner");
 
+INSERT INTO products (name)
+VALUES ("Waffle Maker");
+
+INSERT INTO productss (name)
+VALUES ("Roomba 500");
+
+INSERT INTO products (name)
+VALUES ("Shampoo");
+
+INSERT INTO products (name)
+VALUES ("Coffee Machine");
+
+INSERT INTO products (name)
+VALUES ("Wine Cooler");
+
+INSERT INTO products (name)
+VALUES ("Dog Treats");
+
+INSERT INTO products (name)
+VALUES ("Colored Pencils");
+
+INSERT INTO products (name)
+VALUES ("Duster");
+
+INSERT INTO products (name)
+VALUES ("Makeup Brushes");
